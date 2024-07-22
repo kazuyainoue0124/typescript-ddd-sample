dc08d5eabd0118c7ff208299518636fbd290583a
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-23 08:30:35 +0900

    リポジトリのインターフェースを用いたドメインサービスを実装

--- OnlineBookstore/StockManagement/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
+++ OnlineBookstore/StockManagement/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
@@ -1,18 +1,12 @@
 import { BookId } from 'Domain/models/Book/BookId/BookId';
-import { PrismaClient } from '@prisma/client';
+import { IBookRepository } from 'Domain/models/Book/IBookRepository';
 
-const prisma = new PrismaClient();
-
-// リポジトリを適用せずに実装
 export class ISBNDuplicationCheckDomainService {
+  constructor(private readonly bookRepository: IBookRepository) { }
+
   async execute(isbn: BookId): Promise<boolean> {
     // データベースに問い合わせて重複があるか確認する
-    const duplicateISBNBook = await prisma.book.findUnique({
-      where: {
-        bookId: isbn.value
-      },
-    });
-
+    const duplicateISBNBook = await this.bookRepository.find(isbn);
     const isDuplicateISBN = duplicateISBNBook !== null;
 
     return isDuplicateISBN;
