bd31dba9f4ed77eb254da81d621b58b620364265
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-22 09:21:37 +0900

    リポジトリを適用しない場合のドメインサービスを実装

--- OnlineBookstore/StockManagement/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
+++ OnlineBookstore/StockManagement/src/Domain/services/Book/ISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
@@ -1,9 +1,19 @@
 import { BookId } from 'Domain/models/Book/BookId/BookId';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+// リポジトリを適用せずに実装
 export class ISBNDuplicationCheckDomainService {
   async execute(isbn: BookId): Promise<boolean> {
-    // 本来は、データベースに問い合わせて重複があるか確認する。この章では省略。
-    const isDuplicateISBN = false;
+    // データベースに問い合わせて重複があるか確認する
+    const duplicateISBNBook = await prisma.book.findUnique({
+      where: {
+        bookId: isbn.value
+      },
+    });
+
+    const isDuplicateISBN = duplicateISBNBook !== null;
 
     return isDuplicateISBN;
   }
