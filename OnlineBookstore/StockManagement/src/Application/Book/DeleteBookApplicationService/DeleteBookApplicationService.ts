6a17778d7971ed842e5b3e76b547c3f032cc922c
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-25 08:33:37 +0900

    書籍削除アプリケーションサービスを実装

---
+++ OnlineBookstore/StockManagement/src/Application/Book/DeleteBookApplicationService/DeleteBookApplicationService.ts
@@ -0,0 +1,28 @@
+import { ITransactionManager } from "Application/shared/ITransactionManager";
+import { BookId } from "Domain/models/Book/BookId/BookId";
+import { IBookRepository } from "Domain/models/Book/IBookRepository";
+
+export type DeleteBookCommand = {
+  bookId: string;
+}
+
+export class DeleteBookApplicationService {
+  constructor(
+    private readonly bookRepository: IBookRepository,
+    private readonly transactionManager: ITransactionManager
+  ) { }
+
+  async execute(command: DeleteBookCommand): Promise<void> {
+    await this.transactionManager.begin(async () => {
+      const book = await this.bookRepository.find(new BookId(command.bookId));
+
+      if (!book) {
+        throw new Error('書籍は存在しません');
+      }
+
+      book.delete();
+
+      await this.bookRepository.delete(book.bookId);
+    });
+  }
+}
