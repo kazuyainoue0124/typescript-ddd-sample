a621770f3ccaf2019bd658ba4a456b9f8e75ae7e
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-26 08:54:39 +0900

    DomainEventクラスの作成

---
+++ OnlineBookstore/StockManagement/src/Domain/shared/DomainEvent/DomainEvent.ts
@@ -0,0 +1,32 @@
+import { nanoid } from "nanoid";
+
+export class DomainEvent<
+  T extends Record<string, unknown> = Record<string, unknown>
+> {
+  private constructor(
+    // イベントのID
+    public readonly eventId: string,
+    // イベントの内容
+    public readonly eventBody: T,
+    // イベントの名前
+    public readonly eventName: string,
+    // イベントの発生時刻
+    public readonly occurredOn: Date
+  ) { }
+
+  static create<T extends Record<string, unknown> = Record<string, unknown>>(
+    eventBody: T,
+    eventName: string
+  ): DomainEvent<T> {
+    return new DomainEvent(nanoid(), eventBody, eventName, new Date());
+  }
+
+  static reconstruct<T extends Record<string, unknown>>(
+    eventId: string,
+    eventBody: T,
+    eventName: string,
+    occurredOn: Date
+  ): DomainEvent<T> {
+    return new DomainEvent(eventId, eventBody, eventName, occurredOn);
+  }
+}
