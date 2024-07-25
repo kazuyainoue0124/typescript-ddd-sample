4daa921697547c22d417245c6880ecb67458221e
Kazuya INOUE <bauer24tbp@gmail.com>
2024-07-25 09:04:10 +0900

    eslintに不正な依存関係を防ぐためのルールを追加

--- OnlineBookstore/StockManagement/.eslintrc.js
+++ OnlineBookstore/StockManagement/.eslintrc.js
@@ -18,4 +18,41 @@ module.exports = {
       typescript: {},
     },
   },
+  rules: {
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          // Domain層が依存してはいけない領域
+          {
+            from: './src/Application/**/*',
+            target: './src/Domain/**/!(*.spec.ts|*.test.ts)',
+            message: 'Domain層でApplication層をimportしてはいけません。',
+          },
+          {
+            from: './src/Presentation/**/*',
+            target: './src/Domain/**/!(*.spec.ts|*.test.ts)',
+            message: 'Domain層でPresentation層をimportしてはいけません。',
+          },
+          {
+            from: './src/Infrastructure/**/*!(test).ts',
+            target: './src/Domain/**/!(*.spec.ts|*.test.ts)',
+            message: 'Domain層でInfrastructure層をimportしてはいけません。',
+          },
+          // Application層が依存してはいけない領域
+          {
+            from: './src/Presentation/**/*',
+            target: './src/Application/**/!(*.spec.ts|*.test.ts)',
+            message: 'Application層でPresentation層をimportしてはいけません。',
+          },
+          {
+            from: './src/Infrastructure/**/*',
+            target: './src/Application/**/!(*.spec.ts|*.test.ts)',
+            message:
+              'Application層でInfrastructure層をimportしてはいけません。',
+          },
+        ],
+      },
+    ],
+  },
 };
